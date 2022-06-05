44df7176e2485596450f13745bbcb3973da01fe1
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-05 20:47:04 +0300

    add RegisterComponent to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { RegisterComponent } from '../app/components/authentication/register/register.component';
+
+const routes: Routes = [{
+  path: 'register', component: RegisterComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

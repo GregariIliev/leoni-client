bfd51d5e8f0ec1304a10b69458d58a437f2ed8c6
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-10 21:10:03 +0300

    check localrStorage to authorize all routes

---
+++ src/app/guards/authorization.guard.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthorizationGuard implements CanActivate {
+  constructor() { }
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if (localStorage.getItem('leoni')) {
+      return true
+    } else {
+      return false
+    }
+  }
+}

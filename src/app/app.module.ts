eb3eab0a41dcb71c352d541cf55d4623f3d458de
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-10 20:54:42 +0300

    import LoginComponent and DashboardComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,19 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/authentication/register/register.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './components/authentication/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent
+    LoginComponent,
+    DashboardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
     HttpClientModule
   ],
   providers: [],

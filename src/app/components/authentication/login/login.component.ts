c8506a5aa5145b58d98f6acb0bcc864b9732ac6f
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-10 20:56:05 +0300

    import Router and AuthService

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../../service/auth.service';
 
 @Component({
   selector: 'app-login',

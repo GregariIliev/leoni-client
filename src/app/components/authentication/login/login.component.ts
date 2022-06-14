6faf7e9a7ee7806ae5b9f9d5d0008019934ff4f4
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-15 02:23:53 +0300

    rename AuthService to EmployeeService

--- src/app/components/authentication/login/login.component.ts
+++ src/app/components/authentication/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 
-import { AuthService } from '../../../service/auth.service';
+import { EmployeeService } from 'src/app/service/employee.service';
 
 @Component({
   selector: 'app-login',

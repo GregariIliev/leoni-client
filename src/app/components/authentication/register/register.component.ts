d1b5c4b5ac6322c6dd800d2230038be71a59c9c3
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-15 13:41:49 +0300

    remove imports DepartmentInterface and PositionInterface

--- src/app/components/authentication/register/register.component.ts
+++ src/app/components/authentication/register/register.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validator, Validators } from '@angular/forms';
 
-import { DepartmentInterface } from '../../../interfaces/DepartmentInterface';
-import { PositionInterface } from 'src/app/interfaces/PositionInterface';
-
 import { DepartmentService } from 'src/app/service/department.service';
 
 @Component({

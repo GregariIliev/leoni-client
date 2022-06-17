fd3b2a4061b1d3455c86e97da79b5a271827eeae
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-18 00:46:09 +0300

    create a custom Table Comonent

---
+++ src/app/components/dashboard/table/table.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ChangeDetectionStrategy, Input, OnChanges, SimpleChanges } from '@angular/core';
+
+@Component({
+  selector: 'app-table',
+  templateUrl: './table.component.html',
+  styleUrls: ['./table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class TableComponent implements OnInit, OnChanges {
+ // @Input() view: any;
+
+  constructor() { }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log(changes);
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+}

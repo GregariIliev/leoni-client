2f430c148ce71270e2487c503718fd0393ed50cf
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-18 00:59:27 +0300

    remove import UrlSegment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes, UrlSegment } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { RegisterComponent } from './components/authentication/register/register.component';
 import { LoginComponent } from './components/authentication/login/login.component';

e076d458bd8416d4e58617e951e2d3a63fe5c2ee
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-18 00:49:59 +0300

    create a AuthorizationGuard for routes and child routes

--- src/app/guards/authorization.guard.ts
+++ src/app/guards/authorization.guard.ts
@@ -1,12 +1,21 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthorizationGuard implements CanActivate {
-  constructor() { }
+export class AuthorizationGuard implements CanActivate, CanActivateChild {
+  constructor(private router: Router) { }
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | UrlTree | Observable<boolean | UrlTree> | Promise<boolean | UrlTree> {
+    if (localStorage.getItem('leoni')) {
+      return true
+    } else {
+      this.router.navigateByUrl('login');
+      return false
+    }
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -15,6 +24,7 @@ export class AuthorizationGuard implements CanActivate {
     if (localStorage.getItem('leoni')) {
       return true
     } else {
+      this.router.navigateByUrl('login');
       return false
     }
   }

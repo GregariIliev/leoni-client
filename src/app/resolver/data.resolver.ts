783616ed71e8e20a25c31c700581ac8d0b1043fd
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-18 22:05:53 +0300

    import employeeService departmentService and positionService

---
+++ src/app/resolver/data.resolver.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import {
+  Router, Resolve,
+  RouterStateSnapshot,
+  ActivatedRouteSnapshot
+} from '@angular/router';
+import { forkJoin, map, Observable, of } from 'rxjs';
+
+import { DepartmentService } from '../service/department.service';
+import { EmployeeService } from '../service/employee.service';
+import { PositionService } from '../service/position.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataResolver implements Resolve<boolean> {
+  constructor(
+    private readonly employeeService: EmployeeService,
+    private readonly deparmtentService: DepartmentService,
+    private readonly positionService: PositionService
+  ) { }
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+  }
+}

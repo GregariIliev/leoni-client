004f26b7ddc7a4ecad931de6b162cc7067104231
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-18 22:11:13 +0300

    get statistics form route resolver observable

--- src/app/components/dashboard/statistics/statistics.component.ts
+++ src/app/components/dashboard/statistics/statistics.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.scss']
 })
 export class StatisticsComponent implements OnInit {
+  employeeCount!: any;
+  departmentCount!: any;
+  positionsCount!: any;
 
-  constructor() { }
+  constructor(private readonly route: ActivatedRoute) { }
 
   ngOnInit(): void {
+     this.route.data.subscribe(({ stats }) => {
+       this.employeeCount = stats.employeesCount;
+       this.departmentCount = stats.departmentsCount;
+       this.positionsCount = stats.positionsCount;
+     })
   }
 
 }

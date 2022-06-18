9440209f8f9b287d38bdbace57f4cc3df5a158ef
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-18 22:20:18 +0300

    import and declare EmployeesTable, DepartmentsTable and PositionsTable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,9 +13,11 @@ import { LoginComponent } from './components/authentication/login/login.componen
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { RegisterComponent } from './components/authentication/register/register.component';
 import { SideNavbarComponent } from './components/dashboard/side-navbar/side-navbar.component';
-import { TableComponent } from './components/dashboard/table/table.component';
 import { AdminPanelComponent } from './components/dashboard/admin-panel/admin-panel.component';
 import { StatisticsComponent } from './components/dashboard/statistics/statistics.component';
+import { EmployeesTableComponent } from './components/dashboard/table/employees/employees.table.component';
+import { DepartmentsTableComponent } from './components/dashboard/table/departments/departments-table.component';
+import { PositionsTableComponent } from './components/dashboard/table/positions/positions-table.component';
 
 @NgModule({
   declarations: [
@@ -24,9 +26,11 @@ import { StatisticsComponent } from './components/dashboard/statistics/statistic
     DashboardComponent,
     RegisterComponent,
     SideNavbarComponent,
-    TableComponent,
+    EmployeesTableComponent,
     AdminPanelComponent,
     StatisticsComponent,
+    PositionsTableComponent,
+    DepartmentsTableComponent,
   ],
   imports: [
     CommonModule,

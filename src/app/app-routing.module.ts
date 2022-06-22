0bb1a2cb3a4e836afc6eca911e500f6800d57860
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-22 16:57:49 +0300

    re organize the order of all imports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { AuthorizationGuard } from './guards/authorization.guard';
-import { DataResolver } from './resolver/data.resolver';
 
-import { RegisterComponent } from './components/authentication/register/register.component';
+import { DataResolver } from './interfaces/resolver/data.resolver';
+import { AuthentiacationResolver } from './interfaces/resolver/authentiacation.resolver';
+
 import { LoginComponent } from './components/authentication/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
 
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AdminPanelComponent } from './components/dashboard/admin-panel/admin-panel.component';
+import { StatisticsComponent } from './components/dashboard/statistics/statistics.component';
 import { EmployeesTableComponent } from './components/dashboard/table/employees/employees.table.component';
 import { DepartmentsTableComponent } from './components/dashboard/table/departments/departments-table.component';
 import { PositionsTableComponent } from './components/dashboard/table/positions/positions-table.component';
-import { AdminPanelComponent } from './components/dashboard/admin-panel/admin-panel.component';
-import { StatisticsComponent } from './components/dashboard/statistics/statistics.component';
+import { EmployeeFormComponent } from './components/create/employee-form/employee-from.component';
 import { DepartmentFormComponent } from './components/create/department-form/department-form.component';
 import { PositionFormComponent } from './components/create/position-form/position-form.component';
 

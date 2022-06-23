830741b2afb8cc6f0cd47048c1dea3ec0beb3518
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-24 00:48:36 +0300

    remove all imports without CommonModule, FormsModule and HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,59 +1,25 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common'
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
+import { AppComponent } from './app.component';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
-import { LoginComponent } from './components/authentication/login/login.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { EmployeeFormComponent } from './components/create/employee-form/employee-from.component';
-import { SideNavbarComponent } from './components/dashboard/side-navbar/side-navbar.component';
-import { AdminPanelComponent } from './components/dashboard/admin-panel/admin-panel.component';
-import { StatisticsComponent } from './components/dashboard/statistics/statistics.component';
-import { EmployeesTableComponent } from './components/dashboard/table/employees/employees.table.component';
-import { DepartmentsTableComponent } from './components/dashboard/table/departments/departments-table.component';
-import { PositionsTableComponent } from './components/dashboard/table/positions/positions-table.component';
-import { DepartmentFormComponent } from './components/create/department-form/department-form.component';
-import { PositionFormComponent } from './components/create/position-form/position-form.component';
-
-import { AuthenticationInterceptor } from './interceptor/authentication.interceptor';
-import { EmployeeCardComponent } from './components/employee-card/employee-card.component';
-import { DepartmentCardComponent } from './components/department-card/department-card.component';
-import { PositionCardComponent } from './components/position-card/position-card.component';
-
+import { AuthenticationInterceptor } from './core/interceptor/authentication.interceptor';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    DashboardComponent,
-    EmployeeFormComponent,
-    SideNavbarComponent,
-    EmployeesTableComponent,
-    AdminPanelComponent,
-    StatisticsComponent,
-    PositionsTableComponent,
-    DepartmentsTableComponent,
-    DepartmentFormComponent,
-    PositionFormComponent,
-    EmployeeCardComponent,
-    DepartmentCardComponent,
-    PositionCardComponent,
   ],
   imports: [
-    CommonModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule
   ],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true }
@@ -61,3 +27,5 @@ import { PositionCardComponent } from './components/position-card/position-card.
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+//ng generate module dashboard --route dashboard --module app.module

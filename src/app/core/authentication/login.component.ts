fbf135c9f917dac5d3175363bd8694e780fb14ca
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-24 02:14:52 +0300

    remove import Subject

--- src/app/core/authentication/login.component.ts
+++ src/app/core/authentication/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { BehaviorSubject, Subject } from 'rxjs';
+import { BehaviorSubject } from 'rxjs';
 
 import { AuthService } from './service/auth.service';
 

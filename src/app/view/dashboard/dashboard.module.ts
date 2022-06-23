8b711f8f0e789da583827936ed45e724674e9e46
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-24 02:38:49 +0300

    import and declare StatisticsComponent and SideNavbarComponent

--- src/app/view/dashboard/dashboard.module.ts
+++ src/app/view/dashboard/dashboard.module.ts
@@ -3,11 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { SideNavbarComponent } from './side-navbar/side-navbar.component';
 
 
 @NgModule({
   declarations: [
-    DashboardComponent
+    DashboardComponent,
+    StatisticsComponent,
+    SideNavbarComponent
   ],
   imports: [
     CommonModule,

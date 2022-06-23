7af2f5c1bceeddc98ef50db2f1e31320ef133cc3
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-23 16:16:10 +0300

    create Employee interface

---
+++ src/app/interface/Employee.ts
@@ -0,0 +1,17 @@
+export interface Employee {
+    id: number,
+    firstName: string,
+    middleName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    shift: string,
+    salary: number,
+    address: string,
+    phone: string,
+    position_id: number,
+    department_id: number,
+    admin_id: number,
+    updatedAt: string,
+    createdAt: string,
+}

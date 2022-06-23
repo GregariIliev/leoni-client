a4b4a42c764f9741b7c21e73f5461b91b9408d29
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-23 16:15:28 +0300

    create DepartmentCardComponent

---
+++ src/app/components/department-card/department-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-card',
+  templateUrl: './department-card.component.html',
+  styleUrls: ['./department-card.component.scss']
+})
+export class DepartmentCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

0895c6bcb6995102554a9a135e14b62cae1cd2c9
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-24 02:39:48 +0300

    implement lazy load dashboard module to routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,8 +4,9 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './core/authentication/login.component';
 
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch: 'full'},
-  {path: 'login', component: LoginComponent, title: 'Login'}
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent, title: 'Login' },
+  { path: 'dashboard', loadChildren: () => import('./view/dashboard/dashboard.module').then(m => m.DashboardModule) }
 ];
 
 @NgModule({

eb6f676ec5b7d8884fddd64ffcd581cb29659037
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-24 19:40:34 +0300

    import and implemate to dashboard route DataResolver and AuthorizationGuard

--- src/app/view/dashboard/dashboard-routing.module.ts
+++ src/app/view/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DataResolver } from 'src/app/resolver/data.resolver';
 import { DashboardComponent } from './dashboard.component';
+import { AuthorizationGuard } from 'src/app/guards/authorization.guard';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '', component: DashboardComponent, canActivate: [AuthorizationGuard], resolve: { stats: DataResolver } },
 ];
 
 @NgModule({

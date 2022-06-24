2c291afd615cb1df285b62820e31d178659699e0
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-24 16:29:21 +0300

    move the service to core services

--- src/app/shared/service/department.service.spec.ts
+++ src/app/shared/service/department.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { AuthServiceService } from './auth-service.service';
+import { DepartmentService } from './department.service';
 
-describe('AuthServiceService', () => {
-  let service: AuthServiceService;
+describe('DepartmentService', () => {
+  let service: DepartmentService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(AuthServiceService);
+    service = TestBed.inject(DepartmentService);
   });
 
   it('should be created', () => {

b696ad68996336f4b5c0377bb83156074dce8718
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-24 22:05:36 +0300

    create table module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,9 @@ import { LoginComponent } from './core/authentication/login.component';
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent, title: 'Login' },
-  { path: 'dashboard', loadChildren: () => import('./view/dashboard/dashboard.module').then(m => m.DashboardModule) }
+  { path: 'dashboard', loadChildren: () => import('./view/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: 'table', loadChildren: () => import('./view/table/table.module').then(m => m.TableModule) },
+
 ];
 
 @NgModule({

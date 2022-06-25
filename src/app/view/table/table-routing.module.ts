09f6859589bb8527afd93d7228848e7a4850e373
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-25 18:13:00 +0300

    add resolver to table route

--- src/app/view/table/table-routing.module.ts
+++ src/app/view/table/table-routing.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TableComponent } from './table.component';
 
-const routes: Routes = [{ path: '', component: TableComponent }];
+import { DataResolver } from 'src/app/resolver/data.resolver';
+
+const routes: Routes = [
+  { path: '', component: TableComponent, resolve: { table: DataResolver } }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

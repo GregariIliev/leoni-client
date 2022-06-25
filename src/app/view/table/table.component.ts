7bab13179b341a106c43585cc8ea55c0dbd99599
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-25 18:23:02 +0300

    subscribe to activated route to get table data

--- src/app/view/table/table.component.ts
+++ src/app/view/table/table.component.ts
@@ -8,9 +8,28 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./table.component.scss']
 })
 export class TableComponent implements OnInit {
+  displayedColumns!: string[];
+  data!: any;
 
   constructor(private readonly activatedRoute: ActivatedRoute) { }
   ngOnInit(): void {
+    this.activatedRoute.data.subscribe({
+
+      next: ({ table }) => {
+        if (table.length > 0) {
+          this.displayedColumns = Object.keys(table[0]);
+          this.data = table
+        }
+      },
+      error: (error) => {
+
+      },
+      complete: () => {
+
+      }
+    })
+
   }
+
 
 }

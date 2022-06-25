c9f4dcaf225bf15250586bf4d959271f87314de5
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-25 18:10:38 +0300

    lazy load employee-table department-table position-table routes

--- src/app/view/admin-panel/admin-panel-routing.module.ts
+++ src/app/view/admin-panel/admin-panel-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { AdminPanelComponent } from './admin-panel.component';
 
-const routes: Routes = [{ path: '', component: AdminPanelComponent }];
+const routes: Routes = [
+  { path: '', component: AdminPanelComponent },
+  { path: 'employee-table', loadChildren: () => import('../table/table.module').then(m => m.TableModule) },
+  { path: 'department-table', loadChildren: () => import('../table/table.module').then(m => m.TableModule) },
+  { path: 'position-table', loadChildren: () => import('../table/table.module').then(m => m.TableModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],

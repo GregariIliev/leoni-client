c064191b0a3f98ab4553f7278fb5bb97fc68c39e
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-25 18:07:56 +0300

    remove lazoad admin panel and table

--- src/app/view/dashboard/dashboard-routing.module.ts
+++ src/app/view/dashboard/dashboard-routing.module.ts
@@ -5,9 +5,7 @@ import { DashboardComponent } from './dashboard.component';
 import { AuthorizationGuard } from 'src/app/guards/authorization.guard';
 
 const routes: Routes = [
-  { path: '', component: DashboardComponent, canActivate: [AuthorizationGuard], resolve: { stats: DataResolver } },
-  { path: 'admin-panel', loadChildren: () => import('../admin-panel/admin-panel.module').then(m => m.AdminPanelModule) },
-  { path: 'table', loadChildren: () => import('../table/table.module').then(m => m.TableModule) }
+  { path: '', component: DashboardComponent, canActivate: [AuthorizationGuard], resolve: { stats: DataResolver } }
 ];
 
 @NgModule({

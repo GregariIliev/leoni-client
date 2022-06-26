ac04efcf7124b26bbd49b12af78a252be923ed1e
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-26 17:53:19 +0300

    import and export employee, department and position forms

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,6 +5,9 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
 import { AdminNavbarComponent } from './components/admin-navbar/admin-navbar.component';
+import { EmployeeFormComponent } from './components/employee-form/employee-from.component';
+import { DepartmentFormComponent } from './components/department-form/department-form.component';
+import { PositionFormComponent } from './components/position-form/position-form.component';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -20,8 +23,14 @@ import { MatSelectModule } from '@angular/material/select';
     ],
     declarations: [
         SideNavbarComponent,
-        AdminNavbarComponent
+        EmployeeFormComponent,
+        DepartmentFormComponent,
+        PositionFormComponent
     ],
-    exports: [CommonModule, FormsModule, SideNavbarComponent, AdminNavbarComponent]
+    exports: [
+        EmployeeFormComponent,
+        DepartmentFormComponent,
+        PositionFormComponent
+    ]
 })
 export class SharedModule { }

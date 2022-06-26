fee3498d51678b068d8735482157b12a92b81302
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-26 17:46:22 +0300

    import MatInputModule and MatSelectModule

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,11 +6,15 @@ import { RouterModule } from '@angular/router';
 import { SideNavbarComponent } from './components/side-navbar/side-navbar.component';
 import { AdminNavbarComponent } from './components/admin-navbar/admin-navbar.component';
 
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule,
+        MatInputModule,
+        MatSelectModule
     ],
     declarations: [
         SideNavbarComponent,

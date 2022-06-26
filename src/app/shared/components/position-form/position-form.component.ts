f80c3e257edf8920cfdf75b3bb9f79ec6d7d88f5
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-26 17:51:51 +0300

    create new position form and remove old form

---
+++ src/app/shared/components/position-form/position-form.component.ts
@@ -0,0 +1,48 @@
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+
+import { PositionService } from '../../services/position.service';
+
+@Component({
+  selector: 'app-position-form',
+  templateUrl: './position-form.component.html',
+  styleUrls: ['./position-form.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PositionFormComponent implements OnInit {
+  errorSubject = new BehaviorSubject<any>('');
+  errorMessage = this.errorSubject.asObservable();
+
+  form!: any;
+  shifts!: string[]
+  constructor(private readonly positionService: PositionService, private readonly fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.form = this.initForm();
+    this.shifts = this.form.get('shift').value
+  }
+
+  onSubmit(position: any) {
+    this.positionService.create(position).subscribe({
+      next: (v) => {
+        //vanigate to position or table
+      },
+      error: (e) => {
+        let error = e.error.errors[0].message;
+        this.errorSubject.next(error);
+      },
+      complete: () => { }
+    })
+  }
+
+  initForm(): FormGroup {
+    return this.fb.group({
+      name: ['', [Validators.required]],
+      salaryMultiplayer: ['', [Validators.required, Validators.min(1), Validators.max(2)]],
+      shift: [['A', 'B', 'C', 'R'], [Validators.required]],
+    })
+  }
+
+
+}

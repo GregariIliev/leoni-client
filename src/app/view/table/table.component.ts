8bcb25051f05ab1e70f1c9ac22da8de28763abf6
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-26 17:42:18 +0300

    set change detection to on push

--- src/app/view/table/table.component.ts
+++ src/app/view/table/table.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrls: ['./table.component.scss']
+  styleUrls: ['./table.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TableComponent implements OnInit {
   displayedColumns!: string[];

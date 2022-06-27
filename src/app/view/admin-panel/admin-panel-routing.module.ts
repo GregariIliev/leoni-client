179dfd520b014104eab13aabbcb17970f2f517ba
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-27 15:08:01 +0300

    add all paths to children

--- src/app/view/admin-panel/admin-panel-routing.module.ts
+++ src/app/view/admin-panel/admin-panel-routing.module.ts
@@ -9,15 +9,20 @@ import { AdminPanelComponent } from './admin-panel.component';
 import { EmployeeFormComponent } from 'src/app/shared/components/employee-form/employee-from.component';
 import { DepartmentFormComponent } from 'src/app/shared/components/department-form/department-form.component';
 import { PositionFormComponent } from 'src/app/shared/components/position-form/position-form.component';
+import { EmployeeCardComponent } from 'src/app/shared/components/employee-card/employee-card.component';
 
 const routes: Routes = [
-  { path: '', component: AdminPanelComponent },
-  { path: 'hire-employee', component: EmployeeFormComponent, canActivate: [AuthorizationGuard], resolve: { allDepartments: DataResolver } },
-  { path: 'create-department', component: DepartmentFormComponent, canActivate: [AuthorizationGuard], resolve: { allPositions: DataResolver } },
-  { path: 'create-position', component: PositionFormComponent, canActivate: [AuthorizationGuard] },
-  { path: 'employee-table', loadChildren: () => import('../table/table.module').then(m => m.TableModule) },
-  { path: 'department-table', loadChildren: () => import('../table/table.module').then(m => m.TableModule) },
-  { path: 'position-table', loadChildren: () => import('../table/table.module').then(m => m.TableModule) },
+  { path: '', component: AdminPanelComponent , children: [
+    { path: 'hire-employee', component: EmployeeFormComponent, canActivate: [AuthorizationGuard], resolve: { allDepartments: DataResolver } },
+    { path: 'create-department', component: DepartmentFormComponent, canActivate: [AuthorizationGuard], resolve: { allPositions: DataResolver } },
+    { path: 'create-position', component: PositionFormComponent, canActivate: [AuthorizationGuard] },
+
+    { path: 'employees', loadChildren: () => import('../table/table.module').then(m => m.TableModule) },
+    { path: 'employees/:id', component: EmployeeCardComponent, pathMatch: 'full' },
+    
+    { path: 'departments', loadChildren: () => import('../table/table.module').then(m => m.TableModule) },
+    { path: 'positions', loadChildren: () => import('../table/table.module').then(m => m.TableModule) },
+  ]},
 ];
 
 @NgModule({

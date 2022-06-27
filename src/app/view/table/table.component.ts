daa5e43e8ed8caf24eebfa49e6ea632fb8a6febc
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-27 15:10:27 +0300

    when click on employee redirect to employee card

--- src/app/view/table/table.component.ts
+++ src/app/view/table/table.component.ts
@@ -1,6 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-table',
@@ -12,7 +13,7 @@ export class TableComponent implements OnInit {
   displayedColumns!: string[];
   data!: any;
 
-  constructor(private readonly activatedRoute: ActivatedRoute) { }
+  constructor(private readonly activatedRoute: ActivatedRoute, private readonly router: Router) { }
   ngOnInit(): void {
     this.activatedRoute.data.subscribe({
       next: ({ table }) => {
@@ -21,11 +22,20 @@ export class TableComponent implements OnInit {
 
           this.data = table
 
-          for (const t of table) {
-            t.Positions = t.Positions.map((p: any) => p.name);
+          if (table[0].Positions) {
+            for (const t of table) {
+              t.Positions = t.Positions.map((p: any) => p.name);
+            }
           }
+
         }
       }
     })
+
+  }
+  onClick(row : any){
+    this.router.navigateByUrl(`/admin-panel/employees/${row.id}`);
+    
+    
   }
 }

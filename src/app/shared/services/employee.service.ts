0dd55921acbc25f3226a6cb0de63fdb342cfcd5f
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-28 18:37:42 +0300

    create get by id function

--- src/app/shared/services/employee.service.ts
+++ src/app/shared/services/employee.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, catchError, map, Observable, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
+
 import { Employee } from 'src/app/interface/Employee';
 
 @Injectable({
@@ -19,26 +20,31 @@ export class EmployeeService {
   }
 
   login(admin: any): Observable<any> {
-    return this.http.post<any>(`${this.API}/employees/login`, admin, { withCredentials: true })
+    return this.http.post<any>(`${this.API}/employees/login`, admin, { withCredentials: true });
   }
 
   createEmplyee(employee: any) {
     return this.http.post<any>(`${this.API}/employees/register`, employee);
   }
 
-  update(employee: Employee){
-    return this.http.put<Employee>(`${this.API}/employees/:id`, employee);
+  getById(employeeId: number) {
+    return this.http.get<Employee>(`${this.API}/employees/${employeeId}`);
+  }
+
+  update(employee: Employee, employeeId: number) {
+    return this.http.put<Employee>(`${this.API}/employees/${employeeId}`, employee);
+  }
+
+  delete(employeeId: number) {
+    return this.http.delete<Employee>(`${this.API}/employees/${employeeId}`);
   }
 
   getAll() {
-    return this.http.get<any>(`${this.API}/employees`);
+    return this.http.get<Employee[]>(`${this.API}/employees`);
   }
 
   count() {
-    return this.http.get<any>(`${this.API}/employees/count`)
+    return this.http.get<number>(`${this.API}/employees/count`);
   }
 
-  getById(id: number) {
-    return this.http.get<any>(`${this.API}/employees/${id}`);
-  }
 }

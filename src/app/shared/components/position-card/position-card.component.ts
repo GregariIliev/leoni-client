40e8c3242cca5b44b6ba9e0f68fd09b18d18f5c0
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-29 17:18:07 +0300

    create PositionCardComponent

---
+++ src/app/shared/components/position-card/position-card.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+
+@Component({
+  selector: 'app-position-card',
+  templateUrl: './position-card.component.html',
+  styleUrls: ['./position-card.component.scss']
+})
+export class PositionCardComponent implements OnInit {
+
+  constructor(
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}

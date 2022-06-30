7ba37d2f97bedc48f3eb11158fdb2abc49bb7f17
Gregari Iliev <68861362+GregariIliev@users.noreply.github.com>
2022-06-30 18:18:57 +0300

    set title to table

--- src/app/view/table/table-routing.module.ts
+++ src/app/view/table/table-routing.module.ts
@@ -7,7 +7,7 @@ import { DataResolver } from 'src/app/resolver/data.resolver';
 import { AuthorizationGuard } from 'src/app/guards/authorization.guard';
 
 const routes: Routes = [
-  { path: '', component: TableComponent, canActivate: [AuthorizationGuard], resolve: { table: DataResolver } }
+  { path: '', component: TableComponent, canActivate: [AuthorizationGuard], resolve: { table: DataResolver }, title: 'Table' }
 ];
 
 @NgModule({
